ebb3d6b095cd75dda9edbde1c0b6373c4c90f148
Nam Nguyen <ducnam1719@gmail.com>
2022-06-21 08:34:15 +0700

    done hh-simple-storage

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -2,6 +2,8 @@ require("@nomiclabs/hardhat-waffle")
 require("@nomiclabs/hardhat-etherscan")
 require("dotenv").config()
 require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -9,9 +11,10 @@ require("./tasks/block-number")
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -30,4 +33,12 @@ module.exports = {
         apiKey: ETHERSCAN_API_KEY,
     },
     solidity: "0.8.7",
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-reporter.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "ETH",
+    },
 }
